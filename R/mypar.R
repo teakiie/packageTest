5b2962f8b5bb7d1cd1c37ba33aaa730e3e545c2e
teakiie <teakiie@gmail.com>
2014-09-15 13:33:17 +0200

    Update mypar.R

--- R/mypar.R
+++ R/mypar.R
@@ -1,9 +1,4 @@
-mypar2 <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",mar=c(2.5,2.5,1.6,1.1),mgp=c(1.5,.5,0),...){
-require(RColorBrewer)
-par(mar=mar,mgp=mgp)
-par(mfrow=c(a,b),...)
-palette(brewer.pal(brewer.n,brewer.name))
-}
+
 ##this is for R studio
 mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=2,cex.main=2,cex.axis=1.5,mar=c(5.1,5.1,3.5,2.1),mgp=c(3,1,0),...){
 require(RColorBrewer)
